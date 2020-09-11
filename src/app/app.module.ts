e15e58ef969247f2e45efe080dafd62355dc4fd7
Jorge Isaza <isaza.jorge@hotmail.com>
2020-09-11 08:50:38 -0500

    Añadido: socketio y lista de mensajes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { SocketService } from './core/services/socket.service';
+
+const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
+    ],
   providers: [
+    SocketService,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private socket:Socket) {}
+
+  public mensajes
+  public textoMensaje:string
+  private usuario = "1"
+
+  ngOnInit(){
+    this.socket.connect()
+    console.log("Suscribiendo");
+    
+    this.socket.fromEvent("message").subscribe(message=>{
+      console.log("Mjjj",message)
+      this.mensajes = message
+    })
+  }
+
+  onClickEnviar(){
+    const mensaje = {
+      texto:this.textoMensaje,
+      usuario:this.usuario
+    }
+    this.socket.emit("message",mensaje)
+    this.textoMensaje = ""
+  }
+
+  cambiarUsuario(){
+    this.usuario = "2"
+    console.log("us",this.usuario);
+    
+  }
 
 }
